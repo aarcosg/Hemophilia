2f378715e0e02c2237bb01d8f39d991015ce16f4
Alvaro Arcos <arcos1989@gmail.com>
2015-10-08 14:05:39 +0200

    Bleed member variables declared

--- backend/src/main/java/us/idinfor/hemophilia/backend/Bleed.java
+++ backend/src/main/java/us/idinfor/hemophilia/backend/Bleed.java
@@ -1,6 +1,18 @@
 package us.idinfor.hemophilia.backend;
 
 
+import java.util.Date;
+
 public class Bleed {
-    //TODO
+    
+    Long id;
+    String deviceId;
+    String reason;
+    String severity;
+    String bodyPart;
+    String medication;
+    Integer dose;
+    String lotNumber;
+    Date time;
+    String description;
 }
